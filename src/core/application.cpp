1da397e79bd012677100a50e8ecdd9b0a74f23be
Joakim Wennergren <joakim.wennergren@standbygroup.se>
2023-08-16 10:30:31 +0200

    Windows

---
+++ src/core/application.cpp
@@ -0,0 +1,28 @@
+#include "application.hpp"
+
+/**
+ * @brief Construct a new Application:: Application object
+ *
+ */
+Application::Application()
+{
+}
+
+/**
+ * @brief
+ *
+ * @return int
+ */
+int Application::Run()
+{
+    /*
+    NS::AutoreleasePool *pAutoreleasePool = NS::AutoreleasePool::alloc()->init();
+
+    MyAppDelegate del;
+    UI::ApplicationMain(0, 0, &del);
+
+    pAutoreleasePool->release();
+    */
+
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,106 +1,50 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <iostream>
-#include <filesystem>
-#include <vector>
+#include "CrossWindow/CrossWindow.h"
 
-#define NS_PRIVATE_IMPLEMENTATION
-#define UI_PRIVATE_IMPLEMENTATION
-#define MTL_PRIVATE_IMPLEMENTATION
-#define MTK_PRIVATE_IMPLEMENTATION
-#define CA_PRIVATE_IMPLEMENTATION
-#include <Metal/Metal.hpp>
-#include <UIKit/UIKit.hpp>
-#include <MetalKit/MetalKit.hpp>
-
-class MyMTKViewDelegate : public MTK::ViewDelegate
+void xmain(int argc, const char **argv)
 {
-public:
-    MyMTKViewDelegate(MTL::Device *pDevice)
+    // 🖼️ Create Window Description
+    xwin::WindowDesc windowDesc;
+    windowDesc.name = "Test";
+    windowDesc.title = "My Title";
+    windowDesc.visible = true;
+    windowDesc.width = 640;
+    windowDesc.height = 480;
+
+    bool closed = false;
+
+    // 🌟 Initialize
+    xwin::Window window;
+    xwin::EventQueue eventQueue;
+
+    if (!window.create(windowDesc, eventQueue))
     {
-        device = pDevice;
+        return;
     }
-    virtual ~MyMTKViewDelegate() override;
-    virtual void drawInMTKView(MTK::View *pView) override;
-    MTL::Device *device;
-};
 
-MyMTKViewDelegate::~MyMTKViewDelegate()
-{
-}
+    // 🏁 Engine loop
+    bool isRunning = true;
 
-void MyMTKViewDelegate::drawInMTKView(MTK::View *pView)
-{
-    //_pViewDelegate->renderer = new Renderer(ctx);
-    pView->draw();
-}
+    while (isRunning)
+    {
+        // ♻️ Update the event queue
+        eventQueue.update();
 
-class MyAppDelegate : public UI::ApplicationDelegate
-{
-public:
-    ~MyAppDelegate();
-    bool applicationDidFinishLaunching(UI::Application *pApp, NS::Value *options) override;
-    void applicationWillTerminate(UI::Application *pApp) override;
+        // 🎈 Iterate through that queue:
+        while (!eventQueue.empty())
+        {
+            const xwin::Event &event = eventQueue.front();
 
-private:
-    UI::Window *_pWindow;
-    UI::ViewController *_pViewController;
-    MTK::View *_pMtkView;
-    MTL::Device *_pDevice;
-    MyMTKViewDelegate *_pViewDelegate = nullptr;
-};
+            if (event.type == xwin::EventType::MouseInput)
+            {
+                const xwin::MouseInputData mouse = event.data.mouseInput;
+            }
+            if (event.type == xwin::EventType::Close)
+            {
+                window.close();
+                isRunning = false;
+            }
 
-MyAppDelegate::~MyAppDelegate()
-{
-    _pMtkView->release();
-    _pWindow->release();
-    _pDevice->release();
-    delete _pViewDelegate;
-}
-
-void MyAppDelegate::applicationWillTerminate(UI::Application *pApp)
-{
-}
-
-bool MyAppDelegate::applicationDidFinishLaunching(UI::Application *pApp, NS::Value *options)
-{
-    CGRect frame = UI::Screen::mainScreen()->bounds();
-
-    _pWindow = UI::Window::alloc()->init(frame);
-
-    _pViewController = UI::ViewController::alloc()->init(nil, nil);
-
-    _pDevice = MTL::CreateSystemDefaultDevice();
-
-    std::cout << _pDevice->name()->utf8String() << std::endl;
-
-    _pMtkView = MTK::View::alloc()->init(frame, _pDevice);
-    _pMtkView->setColorPixelFormat(MTL::PixelFormat::PixelFormatBGRA8Unorm_sRGB);
-    _pMtkView->setClearColor(MTL::ClearColor::Make(1.0, 0.0, 0.0, 1.0));
-
-    _pViewDelegate = new MyMTKViewDelegate(_pDevice);
-    _pMtkView->setDelegate(_pViewDelegate);
-
-    UI::View *mtkView = (UI::View *)_pMtkView;
-    mtkView->setAutoresizingMask(UI::ViewAutoresizingFlexibleWidth | UI::ViewAutoresizingFlexibleHeight);
-    _pViewController->view()->addSubview(mtkView);
-    _pWindow->setRootViewController(_pViewController);
-
-    _pWindow->makeKeyAndVisible();
-
-    return true;
-}
-
-int main(int, char **)
-{
-
-    NS::AutoreleasePool *pAutoreleasePool = NS::AutoreleasePool::alloc()->init();
-
-    MyAppDelegate del;
-    UI::ApplicationMain(0, 0, &del);
-
-    pAutoreleasePool->release();
-
-    return 0;
+            eventQueue.pop();
+        }
+    }
 }
