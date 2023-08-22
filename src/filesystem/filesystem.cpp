9dafcec93ef50e8e0fb6003c62c9859636e1b0af
Joakim Wennergren <joakim@bitsanddivs.com>
2023-08-22 23:33:08 +0200

    Fix warning

--- src/filesystem/filesystem.cpp
+++ src/filesystem/filesystem.cpp
@@ -1,7 +1,1 @@
 #include "filesystem.hpp"
-
-#ifdef BUILD_FOR_IOS
-IOSFilesystem::IOSFilesystem()
-{
-}
-#endif
--- src/filesystem/filesystem.hpp
+++ src/filesystem/filesystem.hpp
@@ -30,6 +30,7 @@ namespace Symbios
                 }
                 return resourcePath;
             }
+            return "";
 #endif
         }
     }
