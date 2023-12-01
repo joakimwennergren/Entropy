43399327b3445157d58453976996f3d5c37cc8df
Joakim Wennergren <joakim@bitsanddivs.com>
2023-12-01 21:07:39 +0100

    add files to android

--- android/app/src/main/cpp/main.cpp
+++ android/app/src/main/cpp/main.cpp
@@ -2,6 +2,7 @@
 #include <android/log.h>
 #include <game-activity/GameActivity.cpp>
 #include <game-text-input/gametextinput.cpp>
+#include "shader.h"
 
 extern "C" {
 
@@ -19,7 +20,7 @@ void handle_cmd(android_app *pApp, int32_t cmd) {
             // "game" class if that suits your needs. Remember to change all instances of userData
             // if you change the class here as a reinterpret_cast is dangerous this in the
             // android_main function and the APP_CMD_TERM_WINDOW handler case.
-            //pApp->userData = new Renderer(pApp);
+
             break;
         case APP_CMD_TERM_WINDOW:
             // The window is being destroyed. Use this to clean up your userData to avoid leaking
---
+++ android/app/src/main/cpp/shader.h
@@ -0,0 +1,10 @@
+//
+// Created by Joakim Wennergren on 2023-12-01.
+//
+
+#ifndef ENTROPY_ENGINE_SHADER_H
+#define ENTROPY_ENGINE_SHADER_H
+
+uint32_t * ReadShader(android_app *app, std::string filePath);
+
+#endif //ENTROPY_ENGINE_SHADER_H
