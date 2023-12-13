2b4446a940787a765662710b2818689b0a226d5f
Joakim Wennergren <joakim@bitsanddivs.com>
2023-12-13 22:11:49 +0100

    work work WIP

---
+++ src/graphics/devices/logical_device.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vulkan/vulkan.hpp>
+#include <iostream>
+#include <set>
+#include <string>
+#include <config.hpp>
+
+#include <graphics/surfaces/surface.hpp>
+#include <graphics/queuefamilies/queuefamily.hpp>
+#include <graphics/devices/physical_device.hpp>
+
+using namespace Entropy::Graphics::Surfaces;
+using namespace Entropy::Graphics::QueueFamilies;
+using namespace Entropy::Graphics::Devices;
+
+namespace Entropy
+{
+    namespace Graphics
+    {
+        namespace Devices
+        {
+            class LogicalDevice
+            {
+            public:
+                LogicalDevice(PhysicalDevice physicalDevice, WindowSurface surface);
+                inline VkDevice Get() { return _logicalDevice; };
+
+            private:
+                VkDevice _logicalDevice;
+            };
+        }
+    }
+}
---
+++ src/graphics/queuefamilies/queuefamily.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <vulkan/vulkan.hpp>
+#include <iostream>
+#include <config.hpp>
+
+#include <graphics/instances/vk_instance.hpp>
+
+using namespace Entropy::Graphics::Instances;
+
+namespace Entropy
+{
+    namespace Graphics
+    {
+        namespace QueueFamilies
+        {
+            struct QueueFamilyIndices
+            {
+                std::optional<uint32_t> graphicsFamily;
+                std::optional<uint32_t> presentFamily;
+
+                bool isComplete()
+                {
+                    return graphicsFamily.has_value() && presentFamily.has_value();
+                }
+            };
+
+            class QueueFamily
+            {
+            public:
+                static QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device, VkSurfaceKHR surface);
+                QueueFamily();
+
+            private:
+            };
+        }
+    }
+}
