1f820628da04e9afcb9b53225a138e8e991ca8ec
Joakim Wennergren <joakim@bitsanddivs.com>
2023-12-14 22:04:08 +0100

    Create vulkan resources

--- scripts/build-shaders-windows.bat
+++
@@ -1,2 +0,0 @@
-C:\VulkanSDK\1.3.268.0\Bin\glslc.exe "C:\Entropy-Engine\shaders\basic\basic.vert" -o "C:\Entropy-Engine\shaders\basic\vert.spv"
-C:\VulkanSDK\1.3.268.0\Bin\glslc.exe "C:\Entropy-Engine\shaders\basic\basic.frag" -o "C:\Entropy-Engine\shaders\basic\frag.spv"
