58faea9097285ae863f455b35cc6e7a2a88e51e4
Joakim Wennergren <joakim@bitsanddivs.com>
2024-01-19 18:26:43 +0100

    GLTF

--- src/graphics/primitives/2d/sprite.hpp
+++ src/graphics/primitives/2d/sprite.hpp
@@ -23,30 +23,16 @@ namespace Entropy
                 Sprite();
 
                 Sprite(std::shared_ptr<ServiceLocator> serviceLocator, FT_Bitmap bitmap);
-                /**
-                 * @brief Construct a new Quad object
-                 *
-                 * @param context
-                 */
+
                 Sprite(std::shared_ptr<ServiceLocator> serviceLocator, std::string path);
 
                 Sprite(unsigned char *pixels, int width, int height);
 
-                inline void SetName(std::string name) { this->name = name; };
-
-                inline void SetId(int id) { this->id = id; };
-
-                inline void SetZIndex(int z) { this->zIndex = z; };
-
-                inline void SetRotation(float r, int orientation)
-                {
-                    this->rotationX = r;
-                    this->orientation = orientation;
-                };
-
-                inline void SetColor(float r, float g, float b, float a) { this->color = glm::vec4(r, g, b, a); };
+                VkDescriptorSet _descriptorSet = VK_NULL_HANDLE;
+                VkDescriptorSetLayout _descriptorSetLayout = VK_NULL_HANDLE;
 
             private:
+                void UpdateDescriptorSets();
             };
         }
     }
--- src/renderables/renderable.hpp
+++ src/renderables/renderable.hpp
@@ -25,7 +25,7 @@ namespace Entropy
         public:
             Renderable()
             {
-                this->id = rand() % UINT_MAX;
+                id = rand() % UINT_MAX;
             }
 
             void SetScript(std::string script)
@@ -74,110 +74,6 @@ namespace Entropy
                 return true;
             }
 
-            void UpdateImage(std::shared_ptr<ServiceLocator> serviceLocator)
-            {
-                // Get required depenencies
-                auto logicalDevice = std::dynamic_pointer_cast<LogicalDevice>(serviceLocator->getService("LogicalDevice"));
-                auto physicalDevice = std::dynamic_pointer_cast<PhysicalDevice>(serviceLocator->getService("PhysicalDevice"));
-                auto descriptorPool = std::dynamic_pointer_cast<DescriptorPool>(serviceLocator->getService("DescriptorPool"));
-
-                if (!logicalDevice->isValid())
-                {
-                    spdlog::error("Trying to create buffer with invalid logical device");
-                    return;
-                }
-
-                VkPhysicalDeviceProperties properties{};
-                vkGetPhysicalDeviceProperties(physicalDevice->Get(), &properties);
-
-                VkSamplerCreateInfo samplerInfo{};
-                samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
-                samplerInfo.magFilter = VK_FILTER_LINEAR;
-                samplerInfo.minFilter = VK_FILTER_LINEAR;
-
-                samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-                samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-                samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-                samplerInfo.anisotropyEnable = VK_TRUE;
-                samplerInfo.maxAnisotropy = properties.limits.maxSamplerAnisotropy;
-                samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
-                samplerInfo.unnormalizedCoordinates = VK_FALSE;
-                samplerInfo.compareEnable = VK_FALSE;
-                samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
-                samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
-                samplerInfo.mipLodBias = 0.0f;
-                samplerInfo.minLod = 0.0f;
-                samplerInfo.maxLod = 0.0f;
-
-                if (vkCreateSampler(logicalDevice->Get(), &samplerInfo, nullptr, &_textureSampler) != VK_SUCCESS)
-                {
-                    throw std::runtime_error("failed to create texture sampler!");
-                }
-
-                VkDescriptorSetLayoutBinding samplerLayoutBinding{};
-                samplerLayoutBinding.binding = 1;
-                samplerLayoutBinding.descriptorCount = 1;
-                samplerLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_SAMPLER;
-                samplerLayoutBinding.pImmutableSamplers = nullptr;
-                samplerLayoutBinding.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
-
-                VkDescriptorSetLayoutBinding texturesLayoutBinding{};
-                texturesLayoutBinding.binding = 2;
-                texturesLayoutBinding.descriptorCount = 1;
-                texturesLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE;
-                texturesLayoutBinding.pImmutableSamplers = nullptr;
-                texturesLayoutBinding.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
-
-                std::array<VkDescriptorSetLayoutBinding, 2> bindings = {samplerLayoutBinding, texturesLayoutBinding};
-                VkDescriptorSetLayoutCreateInfo layoutInfo{};
-                layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-                layoutInfo.bindingCount = static_cast<uint32_t>(bindings.size());
-                layoutInfo.pBindings = bindings.data();
-
-                if (vkCreateDescriptorSetLayout(logicalDevice->Get(), &layoutInfo, nullptr, &_descriptorSetLayout) != VK_SUCCESS)
-                {
-                    throw std::runtime_error("failed to create descriptor set layout!");
-                }
-
-                std::vector<VkDescriptorSetLayout> layouts(1, _descriptorSetLayout);
-
-                VkDescriptorSetAllocateInfo allocInfo{};
-                allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
-                allocInfo.descriptorPool = descriptorPool->Get();
-                allocInfo.descriptorSetCount = 1; // MAX_CONCURRENT_FRAMES_IN_FLIGHT;
-                allocInfo.pSetLayouts = layouts.data();
-
-                if (vkAllocateDescriptorSets(logicalDevice->Get(), &allocInfo, &_descriptorSet) != VK_SUCCESS)
-                {
-                    throw std::runtime_error("failed to allocate descriptor sets!");
-                }
-
-                VkDescriptorImageInfo imageInfo{};
-                imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-                imageInfo.imageView = texture->GetImageView();
-                imageInfo.sampler = _textureSampler;
-
-                std::array<VkWriteDescriptorSet, 2> descriptorWrites{};
-
-                descriptorWrites[0].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-                descriptorWrites[0].dstSet = _descriptorSet;
-                descriptorWrites[0].dstBinding = 2;
-                descriptorWrites[0].dstArrayElement = 0;
-                descriptorWrites[0].descriptorType = VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE;
-                descriptorWrites[0].descriptorCount = 1;
-                descriptorWrites[0].pImageInfo = &imageInfo;
-
-                descriptorWrites[1].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-                descriptorWrites[1].dstSet = _descriptorSet;
-                descriptorWrites[1].dstBinding = 1;
-                descriptorWrites[1].dstArrayElement = 0;
-                descriptorWrites[1].descriptorType = VK_DESCRIPTOR_TYPE_SAMPLER;
-                descriptorWrites[1].descriptorCount = 1;
-                descriptorWrites[1].pImageInfo = &imageInfo;
-
-                vkUpdateDescriptorSets(logicalDevice->Get(), static_cast<uint32_t>(descriptorWrites.size()), descriptorWrites.data(), 0, nullptr);
-            }
-
             int zIndex = 0;
             unsigned int id = 0;
             std::string name;
@@ -198,10 +94,6 @@ namespace Entropy
             Texture *texture = nullptr;
             std::unique_ptr<Entropy::Graphics::Buffers::VertexBuffer> vertexBuffer;
             std::unique_ptr<Buffer> indexBuffer;
-
-            // Testing
-            VkDescriptorSet _descriptorSet = VK_NULL_HANDLE;
-            VkDescriptorSetLayout _descriptorSetLayout = VK_NULL_HANDLE;
 
             VkSampler _textureSampler;
 
