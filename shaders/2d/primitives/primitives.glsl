e372e15bcd15a1b498f1a6d8f992cac0115c3313
Joakim Wennergren <joakim.wennergren@databeams.se>
2025-01-12 21:02:04 +0100

    Rounded rect

--- shaders/2d/primitives/primitives.glsl
+++ shaders/2d/primitives/primitives.glsl
@@ -1,40 +1,5 @@
 void RoundedRectangleFrame()
 {
-    /*
-    // pixel and moust coordinates
-    vec2 p = (2.0*gl_FragCoord.xy-inResolution.xy)/inResolution.y;// inUV0;
-
-    // animate the square
-    float si = 0.5;// float s = 0.5 - (radius + (thickness / 10));
-    float ra = 0.1;
-
-    // draw the square
-    float d = sdRoundSquare(p, si, ra);
-
-    // apply colors to it
-    vec4 col = mix(vec4(1.0, 0.0, 0.0, 1.0), vec4(0.0, 1.0, 0.0, 1.0), 1.0-smoothstep(0.0, 0.01, abs(d)));
-
-    outColor = col;
-    */
-
-    /*
-    const vec4 borderColor = vec4(0.0, 1.0, 0.5, 1.0);
-    const vec4 fillColor = vec4(1.0, 0.0, 0.0, 0.0);
-    const float borderThickness = 5.0;
-    const float radius = 12.0;
-    const vec4  u_cornerRadiuses = vec4(10.0, 20.0, 40.0, 60.0);
-    const vec2 rectSize = vec2(300.0, 300.0);
-
-    vec2 pos = rectSize * inUV0;
-
-    float fDist = roundedBoxSDF(pos-rectSize/2.0, rectSize/2.0 - borderThickness/2.0-1.0, u_cornerRadiuses);
-    float fBlendAmount = smoothstep(-1.0, 1.0, abs(fDist) - borderThickness / 2.0);
-
-    vec4 v4FromColor = borderColor;
-    vec4 v4ToColor = (fDist < 0.0) ? fillColor : vec4(0.0);
-    outColor = mix(v4FromColor, v4ToColor, fBlendAmount);
-    */
-
     // =========================================================================
     // Inputs (uniforms)
 
@@ -43,15 +8,15 @@ void RoundedRectangleFrame()
     vec2  u_offset = vec2(20, 20);
 
     float u_edgeSoftness   = 1.0;// How soft the edges should be (in pixels). Higher values could be used to simulate a drop shadow.
-    vec4  u_cornerRadiuses = vec4(0.0, 40.0, 10.0, 0.0);// The radiuses of the corners(in pixels): [topRight, bottomRight, topLeft, bottomLeft]
+    vec4  u_cornerRadiuses = vec4(10.0, 120.0, 120.0, 10.0);// The radiuses of the corners(in pixels): [topRight, bottomRight, topLeft, bottomLeft]
 
     // Border
     float u_borderThickness = 2.0;// The border size (in pixels)
     float u_borderSoftness  = 2.0;// How soft the (internal) border should be (in pixels)
 
     // Shadow
-    float u_shadowSoftness = 4.0;// The (half) shadow radius (in pixels)
-    vec2  u_shadowOffset   = vec2(0.0, 0.0);// The pixel-space shadow offset from rectangle center
+    float u_shadowSoftness = 12.0;// The (half) shadow radius (in pixels)
+    vec2  u_shadowOffset   = vec2(3.0, 3.0);// The pixel-space shadow offset from rectangle center
 
     // Colors
     vec4  u_colorBg     = vec4(0.0, 0.0, 0.0, 0.0);// The color of background
@@ -81,18 +46,6 @@ void RoundedRectangleFrame()
 
     float shadowDistance  = roundedBoxSDF(u_rectCenter-halfSize + u_shadowOffset, halfSize - u_offset, u_cornerRadiuses);
     float shadowAlpha      = 1.0-smoothstep(-u_shadowSoftness, u_shadowSoftness, shadowDistance);
-
-
-    // -------------------------------------------------------------------------
-    // Debug output
-
-    // vec4 debug_sdf = vec4(distance, 0.0, 0.0, 1.0);
-
-    // Notice, that instead simple 'alpha' here is used 'min(u_colorRect.a, alpha)' to enable transparency
-    // vec4 debug_rect_color   = mix(u_colorBg, u_colorRect, min(u_colorRect.a, smoothedAlpha));
-
-    // Notice, that instead simple 'alpha' here is used 'min(u_colorBorder.a, alpha)' to enable transparency
-    // vec4 debug_border_color = mix(u_colorBg, u_colorBorder, min(u_colorBorder.a, min(borderAlpha, smoothedAlpha)) );
 
     // -------------------------------------------------------------------------
     // Apply colors layer-by-layer: background <- shadow <- rect <- border.
